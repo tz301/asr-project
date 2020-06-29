5e0cad9474350516255873cbc3317535d84c6772
ubuntu <tuzhen301@qq.com>
2020-06-29 09:31:03 +0800

    增加模型推理模块, 增加asr模块

---
+++ asr/model.cpp
@@ -0,0 +1,81 @@
+// Created by tz301 on 2020/06/28
+
+#include "asr/model.h"
+
+#include <string>
+
+namespace tz_asr {
+
+Model::Model(const std::string &pb_file,
+             const std::vector<std::string> &input_nodes,
+             const std::vector<std::string> &output_nodes)
+    : input_nodes_(input_nodes), output_nodes_(output_nodes) {
+  session_options_.config.set_use_per_session_threads(true);
+  session_.reset(tensorflow::NewSession(session_options_));
+  tensorflow::GraphDef graph_def;
+  auto default_env = tensorflow::Env::Default();
+  auto ret = tensorflow::ReadBinaryProto(default_env, pb_file, &graph_def);
+  if (!ret.ok()) {
+    std::string err = "Error loading " + pb_file + ": " + ret.error_message();
+    throw std::invalid_argument(err);
+  }
+  session_->Create(graph_def);
+}
+
+kaldi::Matrix<float> Model::Infer(
+    const std::vector<kaldi::Matrix<float>> &inputs) const {
+  auto input_size = inputs.size();
+  auto num_input_nodes = input_nodes_.size();
+
+  if (input_size != num_input_nodes) {
+    std::string err = "Error: need " + std::to_string(num_input_nodes) +
+                      " inputs, only get " + std::to_string(input_size) + ".";
+    throw std::invalid_argument(err);
+  }
+
+  std::vector<std::pair<std::string, tensorflow::Tensor>> tf_inputs;
+  for (size_t i = 0; i < input_size; ++i) {
+    tf_inputs.emplace_back(input_nodes_[i], MatrixToTensor(inputs[i]));
+  }
+
+  std::vector<tensorflow::Tensor> output;
+  auto status = session_->Run(tf_inputs, output_nodes_, {}, &output);
+  if (!status.ok()) {
+    throw std::invalid_argument(status.error_message());
+  } else {
+    return TensorToMatrix(output[0]);
+  }
+}
+
+tensorflow::Tensor Model::MatrixToTensor(
+    const kaldi::Matrix<float> &matrix) const {
+  auto rows = matrix.NumRows();
+  auto cols = matrix.NumCols();
+  tensorflow::TensorShape tensor_shape{rows, cols};
+  tensorflow::Tensor tensor(tensorflow::DT_FLOAT, tensor_shape);
+  auto float_tensor = tensor.tensor<float, 2>();
+
+  for (decltype(rows) i = 0; i < rows; ++i) {
+    for (decltype(cols) j = 0; j < cols; ++j) {
+      float_tensor(i, j) = matrix(i, j);
+    }
+  }
+  return tensor;
+}
+
+kaldi::Matrix<float> Model::TensorToMatrix(
+    const tensorflow::Tensor &tensor) const {
+  auto rows = tensor.shape().dim_size(0);
+  auto cols = tensor.shape().dim_size(1);
+  auto float_tensor = tensor.tensor<float, 2>();
+
+  kaldi::Matrix<float> matrix(rows, cols);
+  for (decltype(rows) i = 0; i < rows; ++i) {
+    for (decltype(cols) j = 0; j < cols; ++j) {
+      matrix(i, j) = float_tensor(i, j);
+    }
+  }
+  return matrix;
+}
+
+}  // namespace tz_asr
---
+++ asr/model.h
@@ -0,0 +1,61 @@
+// Created by tz301 on 2020/06/28
+
+#ifndef ASR_MODEL_H_
+#define ASR_MODEL_H_
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "matrix/kaldi-matrix.h"
+#include "tensorflow/cc/saved_model/loader.h"
+
+namespace tz_asr {
+
+/**
+ * @brief Model.
+ */
+class Model {
+ public:
+  /**
+   * @brief Construct a new Model object.
+   * @param pb_file pb file.
+   * @param input_nodes input node names.
+   * @param output_nodes output node names.
+   */
+  Model(const std::string &pb_file, const std::vector<std::string> &input_nodes,
+        const std::vector<std::string> &output_nodes);
+
+  /**
+   * @brief Inference.
+   * @param inputs mfcc and ivector feature matrix.
+   * @return acoustic prob matrix.
+   */
+  kaldi::Matrix<float> Infer(
+      const std::vector<kaldi::Matrix<float>> &inputs) const;
+
+ private:
+  /**
+   * @brief Convert matrix to tensor.
+   * @param matrix matrix.
+   * @return tensor.
+   */
+  tensorflow::Tensor MatrixToTensor(const kaldi::Matrix<float> &matrix) const;
+
+  /**
+   * @brief Convert tensor to matrix.
+   * @param tensor tensor.
+   * @return matrix.
+   */
+  kaldi::Matrix<float> TensorToMatrix(const tensorflow::Tensor &tensor) const;
+
+  const std::vector<std::string> input_nodes_;    // Input node names.
+  const std::vector<std::string> output_nodes_;   // Output node names.
+  std::unique_ptr<tensorflow::Session> session_;  // tensorflow session.
+  tensorflow::SessionOptions session_options_;    // Session options.
+};
+
+}  // namespace tz_asr
+
+#endif  // ASR_MODEL_H_
