384dc6a8005490e354e0ffc8c06ef5a9bc60d369
ubuntu <tuzhen301@qq.com>
2020-07-03 20:14:14 +0800

    直接测试外部接口asr

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,8 +2,7 @@
 
 #include "gtest/gtest.h"
 
-#include "asr/online_asr.h"
-#include "asr/wave_reader.h"
+#include "asr/asr.h"
 
 /**
  * @brief AsrTest.
@@ -26,50 +25,17 @@ class AsrTest : public ::testing::Test {
  * @brief Test OnlineAsr.
  */
 TEST_F(AsrTest, WaveReaderTest) {
-  tz_asr::PcmData pcm_data(wav_file_);
+  asr::PcmData pcm_data(wav_file_);
   EXPECT_EQ(pcm_data.SampleRate(), 16000u);
   EXPECT_EQ(pcm_data.Data().size(), static_cast<size_t>(134526));
-}
-
-/**
- * @brief Test OnlineFeaturePipeline.
- */
-TEST_F(AsrTest, OnlineFeaturePipelineTest) {
-  std::string ivector_dir = model_dir_ + "/ivector_extractor";
-  tz_asr::OnlineFeaturePipeline feature_pipeline(ivector_dir);
-  tz_asr::PcmData pcm_data(wav_file_);
-
-  for (unsigned i = 0; i < 2; ++i) {
-    feature_pipeline.AcceptWaveform(pcm_data.SampleRate(), pcm_data.Data(),
-                                    true);
-
-    auto mfcc_frames_ready = feature_pipeline.MfccFramesReady();
-    auto ivector_frames_ready = feature_pipeline.IvectorFramesReady();
-    EXPECT_EQ(mfcc_frames_ready, 418);
-    EXPECT_EQ(ivector_frames_ready, 418);
-
-    auto mfcc_dim = feature_pipeline.MfccDim();
-    auto ivector_dim = feature_pipeline.IvectorDim();
-    EXPECT_EQ(mfcc_dim, 40);
-    EXPECT_EQ(ivector_dim, 100);
-
-    kaldi::Vector<float> mfcc(mfcc_dim);
-    kaldi::Vector<float> ivector(ivector_dim);
-    feature_pipeline.GetMfccFrame(0, &mfcc);
-    feature_pipeline.GetIvectorFrame(66, &ivector);
-    EXPECT_TRUE(std::fabs(mfcc(0) - 50.2571) < 0.0001);
-    EXPECT_TRUE(std::fabs(ivector(0) - (-0.136949)) < 0.0001);
-
-    feature_pipeline.Reset();
-  }
 }
 
 /**
  * @brief Test OnlineAsr.
  */
 TEST_F(AsrTest, OnlineAsrTest) {
-  tz_asr::OnlineAsr online_asr(model_dir_);
-  tz_asr::PcmData pcm_data(wav_file_);
+  asr::OnlineAsr online_asr(model_dir_);
+  asr::PcmData pcm_data(wav_file_);
 
   size_t chunk_size = 6400;  // 200ms for 16k wav.
   auto data = pcm_data.Data();
